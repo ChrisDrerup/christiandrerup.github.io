5b3c2cb09f413ecc2258d8bd2c8679f7072a7b63
Christian Drerup <49913673+ChrisDrerup@users.noreply.github.com>
2026-09-11 08:47:25 +0000

    Configure custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://chrisdrerup.github.io',
+  site: 'https://christiandrerup.com',
   integrations: [mdx(), tailwind()]
 });
